4bf9238ec9df0c3848b0910d0f302247092d8f6b
OlamideHawanat123 <raheemhawanat@gmail.com>
2026-01-05 12:12:23 +0100

    Get maximum works with it's test

--- Functions.py
+++ Functions.py
@@ -1,4 +1,6 @@
 def is_even(number):
     return number % 2 == 0
 
+def get_maximum(numbers):
+    return max(numbers)
 
--- functions_test.py
+++ functions_test.py
@@ -1,5 +1,5 @@
 import unittest
-from Functions import is_even
+from Functions import *
 
 
 class MyTestCase(unittest.TestCase):
@@ -7,6 +7,11 @@ class MyTestCase(unittest.TestCase):
 
     def test_something(self):
         self.assertTrue(is_even(44))
+
+    def test_that_get_maximum_returns_the_maximum_number_in_a_list(self):
+        result = get_maximum([12, 34, 90, 12])
+        self.assertEqual(result, 90)
+
 
 
 if __name__ == '__main__':
