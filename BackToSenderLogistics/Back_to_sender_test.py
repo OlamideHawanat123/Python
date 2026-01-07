dc2629b2c6298969b4164ede9a4e7dc06c7d3aca
OlamideHawanat123 <raheemhawanat@gmail.com>
2026-01-07 02:22:22 +0100

    fixed my assertion error

--- BackToSenderLogistics/Back_to_sender_test.py
+++ BackToSenderLogistics/Back_to_sender_test.py
@@ -8,6 +8,15 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(45000, result)
 
 
+    def test_that_back_to_sender_throws_an_error_if_CollectionRateIsNegative(self):
+        self.assertRaises(ValueError, back_to_sender.get_price, -1)
+
+    def test_that_back_to_sender_raises_an_error_if_you_pass_any_argument_that_not_of_type_int(self):
+        self.assertRaises(TypeError, back_to_sender.get_price, "abc")
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
--- BackToSenderLogistics/back_to_sender.py
+++ BackToSenderLogistics/back_to_sender.py
@@ -1,14 +1,23 @@
 amount_per_parcel = [160, 200, 250, 500]
 base_pay = 5000
+
+
 def get_price(number):
+    validate_input(number)
+
     if number < 50:
-         return number * amount_per_parcel[0] + base_pay
-
-    elif number ==  50 and number < 60:
+        return number * amount_per_parcel[0] + base_pay
+    elif number < 60:
         return number * amount_per_parcel[1] + base_pay
-
-    elif number == 60 and number < 70:
+    elif number < 70:
         return number * amount_per_parcel[2] + base_pay
-
     else:
         return number * amount_per_parcel[3] + base_pay
+
+
+def validate_input(number):
+    if not isinstance(number, int):
+        raise TypeError(f"{number} is not a valid number")
+
+    if number < 0:
+        raise ValueError("Number of parcels cannot be negative")
